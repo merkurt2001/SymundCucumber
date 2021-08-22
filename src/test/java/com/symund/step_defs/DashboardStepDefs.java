3d4dcb169908ccc604f3d174bfdc5dfb445b3b65
Ugur Yilmazer <kobra667788@gmail.com>
2021-08-22 18:00:41 +0300

    Dashboard scenario updated

--- src/test/java/com/symund/step_defs/DashboardStepDefs.java
+++ src/test/java/com/symund/step_defs/DashboardStepDefs.java
@@ -1,12 +1,25 @@
 package com.symund.step_defs;
 
 import com.symund.pages.DashboardPage;
+import com.symund.utilities.Driver;
+import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
+import org.junit.Assert;
+
+import java.util.Locale;
 
 public class DashboardStepDefs {
 
     @When("the user navigate to {string} page")
     public void the_user_navigate_to_page(String pageName) {
        new DashboardPage().navigateToPage(pageName);
+    }
+
+    @Then("the user on {string} page")
+    public void theUserOnPage(String pageName) {
+        String title = Driver.get().getCurrentUrl().toLowerCase(Locale.ROOT);
+
+        Assert.assertTrue(title.contains(pageName));
+
     }
 }
--- src/test/resources/features/dashboard.feature
+++ src/test/resources/features/dashboard.feature
@@ -5,6 +5,6 @@ Feature: Dashboard check
     Given the user login with valid credentials
 
   Scenario: Dashboard
-    When the user navigate to "dashboard" page
-    Then the user on dashboard page
+    When the user navigate to "photos" page
+    Then the user on "photos" page
 
