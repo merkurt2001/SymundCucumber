ffeb3cf0dbda3100ae62f346790a5581e28c998e
aytennihal <80786330+aytennihal@users.noreply.github.com>
2021-08-28 06:52:21 -0500

    Revert "some add"

--- src/test/java/com/symund/pages/OnlineStatusPage.java
+++ src/test/java/com/symund/pages/OnlineStatusPage.java
@@ -1,15 +1,9 @@
 package com.symund.pages;
 
 import com.symund.utilities.Driver;
-import org.junit.Assert;
-import org.openqa.selenium.By;
-import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
-
-import java.util.ArrayList;
-import java.util.List;
 
 public class OnlineStatusPage {
 
@@ -18,46 +12,14 @@ public class OnlineStatusPage {
     @FindBy(css = "button.custom-input__emoji-button")
     public WebElement emojiButton;
 
-    @FindBy(css = "form.custom-input__form>input")
-    public WebElement statusMessageBar;
+    @FindBy(css = "form.custom-input__form")
+    public WebElement statusMessage;
 
     @FindBy(xpath = "//button[@class='status-buttons__primary primary']")
     public WebElement setStatus;
 
-    @FindBy(css = "div.set-status-modal")
-    public WebElement OnlineStatusModalPage;
-
-  //  @FindBy(xpath = "//label[@for='user-status-online-status-'" + string + "]")
-    //public List<WebElement> statusMessages;
-
-
-
-    @FindBy(css = " span.predefined-status__message")
-    public List<WebElement> statusMessage;
-
-
-    @FindBy(css = "button.custom-input__emoji-button")
-    public WebElement emojiBox;
-
-    public void getEmoji(String string) {
-        WebElement emoji = Driver.get().findElement(By.xpath("//span[@data-title='" + string + "']/span"));
-        JavascriptExecutor executor = (JavascriptExecutor) Driver.get();
-        executor.executeScript("arguments[0].click();", emoji);
-    }
-
-
-
- //   @FindBy(xpath = "//label[@for='user-status-online-status-'" + statusM + "]")
- //   public List<WebElement> statusM;
-
- /*   public void seeEachOption(List<WebElement> options) {
-        for (WebElement option : options) {
-            Assert.assertTrue(option.isDisplayed());*/
 
 
 
 
-
-
-
-    }
+}
